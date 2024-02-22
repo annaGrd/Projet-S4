66927e149e9ad5c9ef3233335fcdfe6012f387ba
Raphaël Bourgouin <raph.bourgouin@free.fr>
2024-02-22 14:50:13 +0100

    Correction des append

--- Programme.py
+++ Programme.py
@@ -83,13 +83,13 @@ def cells():
     for i in range( qx-1 ):
         for j in range( qy -1 ):
             for k in range( qz -1 ):
-                grid.append( [[ i*edge, (i+1)*edge ], [ j*edge, (j+1)* edge ], [ k*edge, (k+1)*edge ], list() ] )
+                np.append(grid, [[ i*edge, (i+1)*edge ], [ j*edge, (j+1)* edge ], [ k*edge, (k+1)*edge ], list() ])
 
-            grid.append([[ i*edge, (i+1)*edge ], [ j*edge, (j+1)* edge ], [ qz*edge, qz*edge + rz ], list() ])
+            np.append(grid, [[ i*edge, (i+1)*edge ], [ j*edge, (j+1)* edge ], [ qz*edge, qz*edge + rz ], list() ])
 
-        grid.append([[ i*edge, (i+1)*edge ], [ qy*edge, qy*edge + ry ], [ qz*edge, qz*edge + rz ], list() ])
+        np.append(grid, [[ i*edge, (i+1)*edge ], [ qy*edge, qy*edge + ry ], [ qz*edge, qz*edge + rz ], list() ])
 
-    grid.append([[ qx*edge, qx*edge + rx ], [ qy*edge, qy*edge + ry ], [ qz*edge, qz*edge + rz ], list() ])
+    np.append(grid, [[ qx*edge, qx*edge + rx ], [ qy*edge, qy*edge + ry ], [ qz*edge, qz*edge + rz ], list() ])
     return grid
 
 cell = cells()
