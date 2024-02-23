e775f693047455fa5bca928d967f8b0333b294e1
annaGrd <160867313+annaGrd@users.noreply.github.com>
2024-02-23 21:39:44 +0100

    Esthétique et rajout de self.marqueur
    Marqueur sert pour cost et fc même si on n'a pas encore prévu d'endroit pour faire changer self.marqueur de True à False et inversement.

--- noeud.py
+++ noeud.py
@@ -1,16 +1,18 @@
 import numpy as np
 
+
 class Noeud:
-    def __init__(self, x = 0.0, y = 0.0, z = 0.0):
+    def __init__(self, x=0.0, y=0.0, z=0.0):
         self.x = x
         self.y = y
         self.z = z
-        self.block = False # être exploré ou non
+        self.block = False  # être exploré ou non
+        self.marqueur = False  # pour fc, savoir si bloqué pour fc
         self.ci = float("inf")
-        self.voisins = [ np.array( list() ), np.array( list() ) ] # fusionner
+        self.voisins = [np.array(list()), np.array(list())]  # fusionner
 
-    def __eq__( self, other ):
-        if self.x == other.x and self.y == other.y and self.z == other.z :
+    def __eq__(self, other):
+        if self.x == other.x and self.y == other.y and self.z == other.z:
             return True
         else:
             return False
