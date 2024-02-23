507fbd840c76847da9100adc2134c6c9bc56ad5c
Raphaël Bourgouin <raph.bourgouin@free.fr>
2024-02-23 19:48:08 +0100

    Deplacement de la fonction randNode dans le fichier tree.py comme méthode de la classe Tree

--- random_sampling.py
+++ random_sampling.py
@@ -9,7 +9,7 @@ def ellipsoid( x, y, z, a, b ):
 
     return ( x**2 + y**2) / b**2 + z**2 / a**2 <= 1
 
-def ellipse(root, goal, a, b):
+def ellipse_sampling(root, goal, a, b):
     # recherche coordonnées, on peut le faire dès le début non ?
     x = uniform(-b, b)
     y = uniform(-b, b)
@@ -24,17 +24,17 @@ def ellipse(root, goal, a, b):
 
     new = reference_change(root, goal, new)
     newNode = Noeud(new[0], new[1], new[2])
-    if not inGrid(newNode): return ellipse(root, goal, a, b)
+    if not inGrid(newNode): return ellipse_sampling(root, goal, a, b)
 
     return newNode
 
 
-def line_sample(root, goal):
+def line_sampling(root, goal):
     r = uniform(0, 1)
     new = (1-r)*root + r*goal
     newNode = Noeud(new[0], new[1], new[2])
 
-    if not inGrid(newNode): return line_sample(root, goal)
+    if not inGrid(newNode): return line_sampling(root, goal)
 
     return newNode
 
@@ -49,33 +49,3 @@ def uniform_sampling():
     if not inGrid(newNode): return uniform_sampling()
 
     return newNode
-
-
-def randNode( T ):
-
-    xgoal = T.xgoal
-    xo = T.traj[0]
-    Pr = uniform(0, 1)
-    xclose = T.traj[-1]  # récupère le noeud le plus proche de xgoal pour le moment
-
-    if Pr > 1 - alpha :
-
-        root = np.array([xo.x, xo.y, xo.z])
-        goal = np.array([xgoal.x, xgoal.y, xgoal.z])
-
-        return line_sample(root, goal)
-
-    elif Pr <= ( 1 - alpha ) / beta or xclose != xgoal :
-
-        return uniform_sampling()
-
-    else :
-        cmin = norme( xo, xgoal)
-        cbest = xgoal.ci # est-ce bien la distance entre xo et xgoal, pas sur
-        a = cbest / 2
-        b = ( cbest**2 + cmin**2 )**0.5 / 2
-
-        root = np.array( [ xo.x, xo.y, xo.z ] )
-        goal = np.array([xgoal.x, xgoal.y, xgoal.z])
-
-        return ellipse(root, goal, a, b)
--- tree.py
+++ tree.py
@@ -1,4 +1,9 @@
 from noeud import Noeud
+from random import uniform
+import numpy as np
+from random_sampling import ellipse_sampling, uniform_sampling, line_sampling
+from utils_grid import norme
+from constants import alpha, beta
 
 class Tree:
     def __init__(self, noeuds=None, xa = Noeud(), xgoal = Noeud()):
@@ -12,3 +17,31 @@ class Tree:
         self.mem = list()
         self.xa = xa # position du drone
         self.xgoal = xgoal # faire choisir un point d'arriver
+
+    def rand_node(self):
+        xgoal = self.xgoal
+        xo = self.traj[0]
+        Pr = uniform(0, 1)
+        xclose = self.traj[-1]
+
+        if Pr > 1 - alpha:
+
+            root = np.array([xo.x, xo.y, xo.z])
+            goal = np.array([xgoal.x, xgoal.y, xgoal.z])
+
+            return line_sampling(root, goal)
+
+        elif Pr <= (1 - alpha) / beta or xclose != xgoal:
+
+            return uniform_sampling()
+
+        else:
+            cmin = norme(xo, xgoal)
+            cbest = xgoal.ci  # est-ce bien la distance entre xo et xgoal, pas sur
+            a = cbest / 2
+            b = (cbest ** 2 + cmin ** 2) ** 0.5 / 2
+
+            root = np.array([xo.x, xo.y, xo.z])
+            goal = np.array([xgoal.x, xgoal.y, xgoal.z])
+
+            return ellipse_sampling(root, goal, a, b)
