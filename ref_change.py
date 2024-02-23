9f4f7508769fcbd7279cafee02df75e2edd7a76e
annaGrd <160867313+annaGrd@users.noreply.github.com>
2024-02-23 21:31:32 +0100

    Juste de l'esthétique

--- ref_change.py
+++ ref_change.py
@@ -1,36 +1,39 @@
 import numpy as np
 from math import sin, cos, atan
 
-def anglePhi( vect ):
+
+def anglePhi(vect):
 
     if vect[0] != 0 and vect[1] != 0:
 
-        return atan( vect[2] / (( vect[0]**2 + vect[1]**2 )**0.5 ))
-
-    else :
-
-        return 0
-
-def angleTheta( vect ):
-
-    if vect[0] != 0 and vect[1] != 0:
-
-        return 2*atan( vect[1] / ( vect[0] + ( vect[0]**2 + vect[1]**2 )**0.5 ))
+        return atan(vect[2] / ((vect[0]**2 + vect[1]**2)**0.5))
 
     else:
 
         return 0
 
-def rotaPhi( phi, vect ): #phi angle de latitude
 
-    mat = np.array( [ [1, 0, 0 ], [ 0, sin(phi), -cos(phi) ], [ 0, cos(phi), sin(phi)] ] )
+def angleTheta(vect):
+
+    if vect[0] != 0 and vect[1] != 0:
+
+        return 2*atan(vect[1] / (vect[0] + (vect[0]**2 + vect[1]**2)**0.5))
+
+    else:
+
+        return 0
+
+
+def rotaPhi(phi, vect):  # phi angle de latitude
+
+    mat = np.array([[1, 0, 0], [0, sin(phi), -cos(phi)], [0, cos(phi), sin(phi)]])
 
     return vect*mat
 
 
-def rotaTheta( theta, vect ):
+def rotaTheta(theta, vect):
 
-    mat = np.array( [[ sin(theta), -cos(theta), 0 ], [ cos(theta), sin(theta), 0 ], [ 0, 0, 1 ]] )
+    mat = np.array([[sin(theta), -cos(theta), 0], [cos(theta), sin(theta), 0], [0, 0, 1]])
 
     return vect*mat
 
