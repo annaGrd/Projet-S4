2f6fa988074178e52008b8deaa8848f5afe517f7
annaGrd <160867313+annaGrd@users.noreply.github.com>
2024-02-23 21:37:10 +0100

    Ajout de cost
    Modification de certaines spécifications ( parent, ... )
    Certainement des problèmes d'import de certaines parties du programme ( j'ai pas tout suivi :) )

--- Programme.py
+++ Programme.py
@@ -7,209 +7,251 @@ from utils_grid import norme, cells
 
 cell = cells()
 
-def mkeXsi( x ) :
+def mkeXsi(x):
 
     Xsi = list()
     qx = x.x // edge
     qy = x.y // edge
     qz = x.z // edge
-    l = cell[qx][qy][qz][3]
+    nodes = cell[qx][qy][qz][3]
 
-    for e in l :
-        if e != x :
+    for e in nodes:
+        if e != x:
             Xsi.append(e)
 
 
 def main(xa, Xobs, xgoal):
 
-    T = Tree( [xa], Noeud(), Noeud() ) # définir xa et xgoal
+    T = Tree([xa], Noeud(), Noeud())  # définir xa et xgoal
     t = time()
 
-    while norme( T.xa, T.xgoal ) > rprox : # ? le drone proche de xgoal, à modifier en dynamique
-    # On récupère le xa du drone et l'évolution de l'environnement
-        while time() - t < 100: #duree arbitraire
-            T = expAndRew( T )
+    while norme(T.xa, T.xgoal) > rprox:  # ? le drone proche de xgoal, à modifier en dynamique
+        # On récupère le xa du drone et l'évolution de l'environnement
+        while time() - t < 100:  # Durée arbitraire
+            T = expAndRew(T)
 
-        T = plan( T )
-        if norme( T.xa, T.traj[0] ) <= rprox : # ou faire avec le ci de traj[0]
+        T = plan(T)
+        if norme(T.xa, T.traj[0]) <= rprox:  # Ou faire avec le ci de traj[0]
 
             T.traj.pop(0)
             T.Qs = list()
         # envoyer la traj au drone, il va vers xo
-    return #?
+    return  #?
+
+
+def cost(T, x):
+
+    """ Pas des plus optimisés pour le moment, recalcule le ci à chaque fois.
+    En ayant connaissance des changements en amont, on n'aurait pas à tout recalculer.
+    Utiliser blocked ?
+
+    Pas certaine que ma structure récursive fonctionne (retours appréciés)
+    """
+
+    pa = parent(T, x)
+    if pa == T.traj[0]:
+        ci = norme(T.traj[0], pa)
+        x.ci = ci
+        return ci
+
+    elif pa.ci == inf or pa.marqueur:  # pas sûre d'avoir besoin des deux conditions
+        x.ci = inf
+        return inf
+
+    else:
+        ci = cost(T, pa) + norme(x, pa)
+        x.ci = ci
+        return ci
 
 ## Algo 3
 
-def addNode( T, xnew, xclosest, Xnear):
+def addNode(T, xnew, xclosest, Xnear):
 
     xmin = xclosest
-    cmin = cost( xclosest ) + norme( xclosest, xnew )
+    cmin = cost(T, xclosest) + norme(xclosest, xnew)
 
-    for xnear in Xnear :
+    for xnear in Xnear:
 
-        cnew = cost( xnear ) + norme( xnear, xnew )
+        cnew = cost(T, xnear) + norme(xnear, xnew)
 
-        if cnew < cmin and line( xnear, xnew ) :
+        if cnew < cmin and line(xnear, xnew):
             cmin = cnew
             xmin = xnear
 
-    T.Vt.append( xnew )
-    T.Et.append( (xmin, xnew ) ) # faire un dictionnaire
-    xnew.voisins[0].append( xmin )
-    xnew.voisins[1].append( xmin.ci )
-    xmin.voisins[0].append( xnew )
-    xmin.voisins[1].append( xnew.ci )
+    T.Vt.append(xnew)
+    T.Et.append((xmin, xnew))  # faire un dictionnaire
+    xnew.voisins[0].append(xmin)
+    xnew.voisins[1].append(xmin.ci)
+    xmin.voisins[0].append(xnew)
+    xmin.voisins[1].append(xnew.ci)
 
-    # Ajout du noeud dans la case correspondante dans la grille
+    # Ajout du nœud dans la case correspondante dans la grille
     qx = xnew.x // edge
     qy = xnew.y // edge
     qz = xnew.z // edge
-    cell[qx][qy][qz][3].append( xnew )
+    cell[qx][qy][qz][3].append(xnew)
 
     return None
 
 
-## Algo 4
+# Algo 4
 
-def fc( T, xc ):
+def fc(T, xc):
 
     # à modifier si on passe à un xgoal dynamique
-    if xc.marqueur :
+    if xc.marqueur:  # pas sûre que cette condition soit utile avec l'existence de cost()
         return inf
 
     else:
-        return cost( xc ) + norme( xc, T.xgoal )
+        return cost(T, xc) + norme(xc, T.xgoal)
 
 
-def parent( T, voisins, ci ):
+
+def parent(T, x):
+
+    # si on demande le parent de la racine
+    if x == T.traj[0]:
+        return None
+
+    voisins = x.voisins[0]
+    ci = x.voisins[1]
     # prendre celui avec le ci plus petit que x
 
-    for i in range( len(ci) ):
-        ci[i] = fc( T, voisins[i] )
+    for i in range(len(ci)):
+        ci[i] = fc(T, voisins[i])
 
     cmin = ci[0]
     pa = voisins[0]
 
     for voisin in voisins:
 
-        if voisin[1] < cmin :
+        if voisin[1] < cmin:
 
             cmin = voisin[1]
             pa = voisin[0]
 
-    return [ pa, cmin ] # retourne un tuple car pratique pour bestChild
-# mettre en commun des bouts de algo 4 et 5
-def rewireRandNode( T ):
+    return pa
+
+# Mettre en commun des bouts d'algo 4 et 5.
+
+
+def rewireRandNode(T):
 
     t = time()
-    while t - time() < 100 or len( T.Qr ) > 0: # Temps arbitraire
+    while t - time() < 100 or len(T.Qr) > 0:  # Temps arbitraire
 
         xr = T.Qr.pop(0)
-        Xsi = # n'existe pas encore
-        Xnear = findNodesNear( T, xr, Xsi )
+        Xnear = findNodesNear(T, xr)
 
-        for xnear in Xnear :
-            cold = cost( xnear )
-            cnew = cost( xr ) + norme( xr, xnear )
+        for xnear in Xnear:
+            cold = cost(T, xnear)
+            cnew = cost(T, xr) + norme(xr, xnear)
 
-            if cnew < cold and line( xr, xnear ):
-                T.Et.append( (xr, xnear) )
-                pa = parent( T, xnear.voisins[0], xnear.voisins[1] )[0]
+            if cnew < cold and line(xr, xnear):
+                T.Et.append((xr, xnear))
+                pa = parent(T, xnear)
 
-                if (xnear, pa) in T.Et :
-                    T.Et.remove( (xnear, pa) )
-                else: # volontairement un else pour déclencher une erreur si cette arête n'existait pas
-                    T.Et.remove( (pa, xnear) )
+                if (xnear, pa) in T.Et:
+                    T.Et.remove((xnear, pa))
+                else:  # volontairement un else pour déclencher une erreur si cette arête n'existait pas
+                    T.Et.remove((pa, xnear))
 
-                if xnear not in T.Qr : # cette condition est censée être toujours vraie
-                    T.Qr.append( xnear )
-                xr.voisins[0].append( xnear )
-                xr.voisins[1].append( cnew )
+                if xnear not in T.Qr:  # cette condition est censée être toujours vraie
+                    T.Qr.append(xnear)
+                xr.voisins[0].append(xnear)
+                xr.voisins[1].append(cnew)
     return T
 
-## Algo 5
+# Algo 5
 
-def rewireRoot( T ):
 
-    if len( T.Qs ) == 0:
-        T.Qs.append( T.traj[0] )
+def rewireRoot(T):
+
+    if len(T.Qs) == 0:
+        T.Qs.append(T.traj[0])
         T.mem = list()
 
     t = time()
-    while t - time() < 100 or len( T.Qs ) > 0:
+    while t - time() < 100 or len(T.Qs) > 0:
 
         xs = T.Qs.pop(0)
-        Xnear = findNodesNear( T, xs, Xsi )
+        Xnear = findNodesNear(T, xs)
 
-        for xnear in Xnear :
+        for xnear in Xnear:
 
-            cold = cost( xnear )
-            cnew = cost( xs ) + norme( xs, xnear )
+            cold = cost(T, xnear)
+            cnew = cost(T, xs) + norme(xs, xnear)
 
-            if cnew < cold and line( xs, xnear ):
+            if cnew < cold and line(xs, xnear):
 
-                T.Et.append( (xs, xnear) )
-                pa = parent( T, xnear.voisins[0], xnear.voisins[1] )
+                T.Et.append((xs, xnear))
+                pa = parent(T, xnear)
 
-                if (xnear, pa) in T.Et :
-                    T.Et.remove( (xnear, pa) )
+                if (xnear, pa) in T.Et:
+                    T.Et.remove((xnear, pa))
 
                 else:
-                    T.Et.remove( (pa, xnear) )
+                    T.Et.remove((pa, xnear))
 
-                xs.voisins[0].append( xnear )
-                xs.voisins[1].append( cnew )
+                xs.voisins[0].append(xnear)
+                xs.voisins[1].append(cnew)
 
-            if xnear not in T.mem : # à ajuster quand on gérera les obstacles dynamiques
-                T.Qs.append( xnear )
-                T.mem.append( xnear )
+            if xnear not in T.mem:  # à ajuster quand on gérera les obstacles dynamiques
+                T.Qs.append(xnear)
+                T.mem.append(xnear)
 
     return T
 
-## Algo 6
+# Algo 6
 
-def bestChild( T, x ):
+
+def bestChild(T, x):
 
     v = x.voisins[0]
     c = x.voisins[1]
 
-    rg = c.index(parent( T, x.voisins[0], x.voisins[1]) )
-    v.pop( rg ) # retire le parent
-    c.pop( rg ) # et son coût
+    rg = v.index(parent(T, x))
+    v.pop(rg)  # retire le parent
+    c.pop(rg)  # et son coût
+    xTempo = Noeud()
+    xTempo.voisins.append(v)
+    xTempo.ci.append(c)
 
-    return parent(T, v, c) # on récupère donc le deuxième meilleur voisin
+    return parent(T, xTempo)  # on récupère donc le deuxième meilleur voisin
 
-def deadEnd( T, x ):
-    if len( x.voisins[0] ) < 2 : # si n'a qu'un voisin ( juste son parent )
+
+def deadEnd(T, x):
+    if len(x.voisins[0]) < 2:  # si n'a qu'un voisin (juste son parent)
         return True
 
-    else :
-        pa = parent( T, x, x.ci )
-        enfant = x.voisins[0] # référence partagée ?
-        enfant.remove( pa ) # obtient liste des noeuds enfants
+    else:
+        pa = parent(T, x)
+        enfant = x.voisins[0]  # référence partagée ?
+        enfant.remove(pa)  # obtient liste des nœuds enfants
 
-        for v in enfant :
-            if not v.ci == inf : # si un enfant n'est pas marqué
+        for v in enfant:
+            if not v.ci == inf:  # si un enfant n'est pas marqué
                 return False
 
-        return True # tous les enfants sont marqués
+        return True  # tous les enfants sont marqués
 
-def plan( T ):
 
-    if norme( T.traj[-1], T.xgoal) < rprox :
+def plan(T):
+
+    if norme(T.traj[-1], T.xgoal) < rprox:
         T.traj[-1] = T.xgoal
         # chemin toujours accessible ? check les ci et compare les arêtes
 
     else:
-        path = [ T.traj[0] ]
-        while deadEnd( T, path[-1] ) :
+        path = [T.traj[0]]
+        while deadEnd(T, path[-1]):
 
-            path.append( T, bestChild( path[-1] ) )
+            path.append(bestChild(T, path[-1]))
 
         path[-1].block = True
-        if fc( path[-1] ) == fc( T, T.traj[-1] ):
+        if fc(T, path[-1]) == fc(T, T.traj[-1]):
 
-        # comparer les arêtes, voir si elles ont changé. regarder les ci, voir si l'un est infini
-        """faire une diff de norme entre xa,xgoal et xk,goal"""
+            # Comparer les arêtes, voir si elles ont changé. Regarder les ci, voir si l'un est infini
+            """faire une diff de norme entre xa,xgoal et xk,goal"""
 
     return T
