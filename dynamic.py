e2e23f10dd7f91bafdf98d207c42aa17b61f264d
Gouédard <anna.gouedard@gmail.com>
2024-02-25 21:24:59 +0100

    Début de la gestion dynamique

--- dynamic.py
+++ dynamic.py
@@ -1,9 +1,10 @@
-from tree import Tree
 from noeud import Noeud
-from constants import update_time, safety_radius
-
+from constants import update_time, safety_radius, edge
+from utils_grid import list_indices_at_range, norme
 
 """obstacles est une liste contenant les obstacles dynamiques de la forme [x, y, z, vitesse]"""
+
+
 def update_block(T, obstacles):
 
     # on récupère tous les noeuds marqués
@@ -14,7 +15,7 @@ def update_block(T, obstacles):
 
     # on récupère tous les noeuds dans la range rb de chaque obstacle
     for obs in obstacles:
-        x_inrange = calcul_inrange(obs)
+        x_inrange = calcul_inrange(T, obs)
 
         # si le nœud est déjà marqué, il le restera, sinon, il le devient
         for x in x_inrange:
@@ -26,5 +27,25 @@ def update_block(T, obstacles):
         x.block = False
 
 
-def calcul_inrange(obs):
+def calcul_inrange(T, obs):
     rb = update_time * obs[3] + safety_radius
+
+    qx = int(obs[0] // edge)
+    qy = int(obs[1] // edge)
+    qz = int(obs[2] // edge)
+
+    if qx == T.nbcellx: qx -= 1
+    if qy == T.nbcelly: qy -= 1
+    if qz == T.nbcellz: qz -= 1
+    node_inrange = list(T.cell[qx, qy, qz])
+
+    gap = list_indices_at_range(1)
+    for abscissa, ordinate, altitude in gap:
+        if not (0 <= qx + abscissa < T.nbcellx and 0 <= qy + ordinate < T.nbcelly and 0 <= qz + altitude < T.nbcellz): continue
+        cell = T.cell[qx + abscissa][qy + ordinate][qz + altitude]
+        node_inrange.extend(cell)
+
+    for x in node_inrange:
+        if norme(x, Noeud(obs[0], obs[1], obs[2])) > rb: node_inrange.remove(x)
+
+    return node_inrange
